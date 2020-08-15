ae67792d598ee205c0a1b253c47db8bd4f333d98
Jaxon1517 <James.hartley@csedge.org>
2020-08-15 17:19:46 -0400

    survey

---
+++ app/src/main/java/com/example/scheduler/ActivityPosts.java
@@ -0,0 +1,30 @@
+package com.example.scheduler;
+
+import android.widget.LinearLayout;
+
+public class ActivityPosts {
+    String Title;
+    String Desc;
+
+    public ActivityPosts(){}
+
+    public ActivityPosts(String Title, String Desc){
+        this.Title = Title;
+        this.Desc = Desc;
+    }
+    public String getTitle() {
+        return Title;
+    }
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public String getDesc() {
+        return Desc;
+    }
+    public void setDesc(String Desc) {
+        this.Desc = Desc;
+    }
+
+}
+
