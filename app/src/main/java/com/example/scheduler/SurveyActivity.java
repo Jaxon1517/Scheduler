827590f0383db1439735a9437ca6ffa54ef27bd9
devauntea <devauntea.thompson@csedge.com>
2020-08-16 11:19:07 -0400

    commit

--- app/src/main/java/com/example/scheduler/SurveyActivity.java
+++ app/src/main/java/com/example/scheduler/SurveyActivity.java
@@ -2,20 +2,37 @@ package com.example.scheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class SurveyActivity extends AppCompatActivity {
 
-    EditText wakeUpEditText;
-    EditText sleepEditText;
-    Button nextButton;
+    TextView wakeUpEditText;
+    TextView sleepEditText;
+    TextView nextButton;
+    TextView Timer;
+    int t1hour, t1minute;
+    TextView Timer2;
+    int t1hour2, t1minute2;
+    TextView Timer3;
+    int t1hour3, t1minute3;
+    TextView Timer4;
+    int t1hour4, t1minute4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +42,149 @@ public class SurveyActivity extends AppCompatActivity {
         wakeUpEditText = findViewById(R.id.wakeUpEditText);
         sleepEditText = findViewById(R.id.sleepEditText);
         nextButton = findViewById(R.id.nextButton);
+        Timer = findViewById(R.id.sleepTime);
+        Timer2 = findViewById(R.id.wakeUpEditText);
+        Timer3 = findViewById(R.id.StartWorkTIme);
+        Timer4 = findViewById(R.id.endWorkTime);
+
+        Timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        SurveyActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                t1hour = hourOfDay;
+                                t1minute = minute;
+
+                                String time = t1hour + ":" + t1minute;
+                                SimpleDateFormat f24Hours = new SimpleDateFormat(
+                                        "HH:mm"
+                                );
+                                try {
+                                    Date date = f24Hours.parse(time);
+                                    SimpleDateFormat f12Hours = new SimpleDateFormat(
+                                            "hh:mm aa"
+                                    );
+                                    Timer.setText(f12Hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.updateTime(t1hour,t1minute);
+                timePickerDialog.show();
+            }
+        });
+
+        Timer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        SurveyActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                t1hour2 = hourOfDay;
+                                t1minute2 = minute;
+
+                                String time = t1hour2 + ":" + t1minute2;
+                                SimpleDateFormat f24Hours = new SimpleDateFormat(
+                                        "HH:mm"
+                                );
+                                try {
+                                    Date date = f24Hours.parse(time);
+                                    SimpleDateFormat f12Hours = new SimpleDateFormat(
+                                            "hh:mm aa"
+                                    );
+                                    Timer2.setText(f12Hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.updateTime(t1hour2,t1minute2);
+                timePickerDialog.show();
+            }
+        });
+
+        Timer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        SurveyActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                t1hour3 = hourOfDay;
+                                t1minute3 = minute;
+
+                                String time = t1hour3 + ":" + t1minute3;
+                                SimpleDateFormat f24Hours = new SimpleDateFormat(
+                                        "HH:mm"
+                                );
+                                try {
+                                    Date date = f24Hours.parse(time);
+                                    SimpleDateFormat f12Hours = new SimpleDateFormat(
+                                            "hh:mm aa"
+                                    );
+                                    Timer3.setText(f12Hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.updateTime(t1hour3,t1minute3);
+                timePickerDialog.show();
+            }
+        });
+
+        Timer4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        SurveyActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                t1hour4 = hourOfDay;
+                                t1minute4 = minute;
+
+                                String time = t1hour4 + ":" + t1minute4;
+                                SimpleDateFormat f24Hours = new SimpleDateFormat(
+                                        "HH:mm"
+                                );
+                                try {
+                                    Date date = f24Hours.parse(time);
+                                    SimpleDateFormat f12Hours = new SimpleDateFormat(
+                                            "hh:mm aa"
+                                    );
+                                    Timer4.setText(f12Hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.updateTime(t1hour4,t1minute4);
+                timePickerDialog.show();
+            }
+        });
+
+
+
     }
 
     public void nextButtonPressed(View view) {
