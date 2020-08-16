27faecaea4c26b4fa1f93f4cb527a2117604ad93
devauntea <devauntea.thompson@csedge.com>
2020-08-16 01:48:15 -0400

    commit

--- app/src/main/java/com/example/scheduler/HomeActivity.java
+++ app/src/main/java/com/example/scheduler/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.scheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+    }
+
+    public void goHome(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+    public void goTask(View view){
+        Intent intent = new Intent(this, TasksActivity.class);
+        startActivity(intent);
+    }
+    public void goAddTask(View view){
+        Intent intent = new Intent(this, AddTasksActivity.class);
+        startActivity(intent);
     }
 }
