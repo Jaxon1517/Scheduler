852d2e7812724285344463a86a236a13cb23333a
Jaxon1517 <James.hartley@csedge.org>
2020-08-15 23:01:40 -0400

    survey

--- app/src/main/java/com/example/scheduler/AboutActivity.java
+++ app/src/main/java/com/example/scheduler/AboutActivity.java
@@ -1,27 +1,37 @@
 package com.example.scheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.github.appintro.AppIntro;
+import com.github.appintro.AppIntroFragment;
 
 public class AboutActivity extends AppIntro {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about2);
+        //adds first of intro slides
+        addSlide(AppIntroFragment.newInstance(
+                "The title of your slide",
+                "A description that will be shown on the bottom",
+                R.drawable.planiitlogo,
+                ContextCompat.getColor(getApplicationContext(),R.color.mainRed)));
     }
     @Override
     public void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
         // Decide what to do when the user clicks on "Skip"
+        startActivity(new Intent(getApplicationContext(),SurveyActivity.class));
+        finish();
 
     }
 
@@ -29,5 +39,7 @@ public class AboutActivity extends AppIntro {
      public void onDonePressed(Fragment currentFragment ) {
         super.onDonePressed(currentFragment);
         // Decide what to do when the user clicks on "Done"
+        startActivity(new Intent(getApplicationContext(),SurveyActivity.class));
+        finish();
     }
 }
