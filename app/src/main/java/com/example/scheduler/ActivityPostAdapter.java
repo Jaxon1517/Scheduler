29c0b3e864e1e3fd7c7f000837f8ce2ffe1a33bf
Jaxon1517 <James.hartley@csedge.org>
2020-08-15 21:53:39 -0400

    survey

--- app/src/main/java/com/example/scheduler/ActivityPostAdapter.java
+++ app/src/main/java/com/example/scheduler/ActivityPostAdapter.java
@@ -7,13 +7,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.TextClock;
 import android.widget.TextView;
 import java.util.ArrayList;
 
-//public class ActivityPostAdapter extends ArrayAdapter<ActivityPosts> {
-//    public TaskPostAdapter(Context context, ArrayList<ActivityPosts> list) {
-//        super(context, 0, list);
-//    }
-//
-//
-//}
+public class ActivityPostAdapter extends ArrayAdapter<ActivityPosts> {
+    public ActivityPostAdapter(Context context, ArrayList<ActivityPosts> list) {
+        super(context, 0, list);
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        convertView = LayoutInflater.from(getContext()).inflate(R.layout.a_list_item, parent, false);
+        ActivityPosts activityMsg = getItem(position);
+
+        TextView titleTextView = convertView.findViewById(R.id.titleTextView);
+        titleTextView.setText(activityMsg.getTitle());
+
+        TextView DescTextView = convertView.findViewById(R.id.descTextView);
+        DescTextView.setText(activityMsg.getTitle());
+
+
+
+
+        return convertView;
+
+    }
+
+
+
+}
