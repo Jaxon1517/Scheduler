8d6917f0c3c77873cfc0c0112cd22e7be428b866
Jaxon1517 <James.hartley@csedge.org>
2020-08-15 23:10:27 -0400

    survey

--- app/src/main/java/com/example/scheduler/MainActivity.java
+++ app/src/main/java/com/example/scheduler/MainActivity.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 
-import com.airbnb.lottie.LottieAnimationView;
-
 public class MainActivity extends AppCompatActivity {
 
     private static int Next = 3000;
